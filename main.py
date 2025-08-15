69374b965a35219a14ec46cdd706a69c18a185f5
warrenboor01-ship-it <warrenboor01@gmail.com>
2025-08-15 09:00:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ bot = Bot(token=BOT_TOKEN)
 print("Bot started!")
 
 while True:
-    try:
-        bot.send_message(chat_id=CHAT_ID, text="@EagleMEVBot For free money while you sleep")
+    try:l
+        bot.send_message(chat_id=CHAT_ID, text="@EagleMEVBot For free money while you sleep🤑
+        dm SolTradess for support")
         print("Message sent!")
     except Exception as e:
         print(f"Error sending message: {e}")
